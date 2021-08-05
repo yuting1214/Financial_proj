4126211876b5c39843b18d8e827d2595f532869f
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-08-05 15:17:36 +0800

    Update Insert_from_another_table.sql

--- CRUD_pattern/Insert_from_another_table.sql
+++ CRUD_pattern/Insert_from_another_table.sql
@@ -1,2 +1,13 @@
 # Select into
+## The SELECT INTO statement copies data from one table into a new table
+SELECT *
+INTO newtable [IN externaldb]
+FROM oldtable
+WHERE condition;
 
+# INSERT INTO
+## The INSERT INTO SELECT statement copies data from one table and inserts it into another table.
+INSERT INTO table2 (column1, column2, column3, ...)
+SELECT column1, column2, column3, ...
+FROM table1
+WHERE condition;
