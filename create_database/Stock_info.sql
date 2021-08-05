5818451e69f036cc7ef433598995c4f309f8162d
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-08-05 14:33:51 +0800

    Delete Stock_info.sql

--- create_database/Stock_info.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Stock_info (
-  Stock_id VARCHAR(6) PRIMARY KEY,
-  Stock_name VARCHAR(10) NOT NULL,
-  Industry_id VARCHAR(2) NOT NULL,
-  Industry_name VARCHAR(10) NOT NULL,
-  List_type VARCHAR(2) NOT NULL
-);
