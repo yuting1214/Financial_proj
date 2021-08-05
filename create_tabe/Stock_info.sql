6eb99d73443e0aa493b12b52c3815fd25f16a7d0
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-08-05 14:33:44 +0800

    Create Stock_info.sql

---
+++ create_tabe/Stock_info.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Stock_info (
+  Stock_id VARCHAR(6) PRIMARY KEY,
+  Stock_name VARCHAR(10) NOT NULL,
+  Industry_id VARCHAR(2) NOT NULL,
+  Industry_name VARCHAR(10) NOT NULL,
+  List_type VARCHAR(2) NOT NULL
+);
