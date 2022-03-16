e74b4d192ed6969aa5bd4aecba99d95bf6b3c264
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2022-03-17 05:50:45 +0800

    Update Stock_category.sql

--- create_tabe/Stock_category.sql
+++ create_tabe/Stock_category.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Stock_category (
   Source VARCHAR(10) NOT NULL,
   Industry_name VARCHAR(10) NOT NULL,
-  Stream VARCHAR(10) NOT NULL,
+  Stream VARCHAR(10),
   Subindustry_name VARCHAR(50) NOT NULL,
   Stock_id VARCHAR(6),
-  Stock_name VARCHAR(10) NOT NULL
+  Stock_name VARCHAR(50) NOT NULL
 );
