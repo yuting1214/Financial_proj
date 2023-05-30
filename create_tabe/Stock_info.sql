3dfed5f34479330c70a48c61ed8d82023660cf7e
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2023-05-30 15:39:10 -0400

    Update Stock_info.sql

--- create_tabe/Stock_info.sql
+++ create_tabe/Stock_info.sql
@@ -3,5 +3,6 @@ CREATE TABLE Stock_info (
   Stock_name VARCHAR(10) NOT NULL,
   Industry_id VARCHAR(2) NOT NULL,
   Industry_name VARCHAR(10) NOT NULL,
-  List_type VARCHAR(2) NOT NULL
+  List_type VARCHAR(2) NOT NULL,
+  List_status VARCHAR(2) NOT NULL
 );
